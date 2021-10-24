198851918df9c8cba0ea950426c5fb9b773d121c
Semih <s.semih.sahan@hotmail.com>
2021-10-25 00:44:55 +0300

    updated

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -8,14 +8,25 @@ import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HrmsApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(HrmsApplication.class, args);
 		//System.out.println(uploadResult.get("url"));
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlamaio.hrms"))                                     
+          .build();                                           
+    }
 	
 
 }
